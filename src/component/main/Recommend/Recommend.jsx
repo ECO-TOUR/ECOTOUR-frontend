fd6f560443f080ff7982d7911603a9d298b42fa6
jpg723 <86431761+jpg723@users.noreply.github.com>
2024-08-19 19:38:36 +0900

    [FEAT] detail 페이지 이동 구현

--- src/component/main/Recommend/Recommend.jsx
+++ src/component/main/Recommend/Recommend.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import * as S from './Recommend.style'
 import exampleImage from '../../../assets/example1.png'; // 이미지 파일을 import
+import { useNavigate } from 'react-router-dom';
 
 function Recommend() {
 
   const contents = [1, 2, 3, 4, 5];
+  const navigate = useNavigate();
+  function onClickBox(){
+    navigate('/detail');
+  }
+  
   return (
     <S.Component>
         {contents.map((content, index) => (
-            <S.ContentBox>
+            <S.ContentBox onClick={onClickBox}>
                 <S.ContentImage src={exampleImage}/>
                 <S.ContentName>순천만습지</S.ContentName>
                 <S.ContentRegion>전라남도 순천시</S.ContentRegion>
