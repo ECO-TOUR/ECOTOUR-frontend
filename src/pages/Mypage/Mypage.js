3de1c99c71249704ce30f45db5cc8c5dda9daa3f
jpg723 <86431761+jpg723@users.noreply.github.com>
2024-09-14 16:34:04 +0900

    [FEAT] 회원탈퇴, 로그아웃, 문의 구현

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -29,7 +29,6 @@ const Nickname = styled.span`
 `;
 const FirstLine = styled.span`
   color: #333;
-  font-family: ABeeZee;
   font-size: 19px;
   font-style: normal;
   font-weight: 600;
@@ -37,53 +36,64 @@ const FirstLine = styled.span`
 const SecondLine = styled.div`
   margin-top: 6px;
   color: #676767;
-  font-family: ABeeZee;
   font-size: 13px;
   font-style: normal;
   font-weight: 400;
 `;
+
+// 내가 쓴 글 container
 const PostList = styled.div`
-  padding: 12px 16px;
+  padding: 12px 0px;
+  margin-top: 10px;
 `;
+
 const PostTitle = styled.div`
   display: flex;
   justify-content: space-between;
-  height: 25px;
 `;
+
+// 내가 쓴 글, 서비스 관리
 const Title = styled.div`
   color: #333;
-  font-family: ABeeZee;
   font-size: 20px;
-  font-style: normal;
-  font-weight: 900;
+  font-weight: 700;
+  padding: 0 30px;
+  margin-bottom: 20px;
 `;
+
+// 더보기
 const StyledLink = styled.a`
   color: #333;
   text-decoration: none;
   height: 100%;
+  font-size: 14px;
+  font-weight: 400;
+  padding: 0px 30px;
 `;
 const Settings = styled.div`
   height: 120px;
   padding: 12px 12px;
 `;
+
+// 서비스 관리 div
 const Line = styled.div`
   align-items: center;
   height: 30px;
-  max-width: 386px;
   display: flex;
   justify-content: space-between;
   font-size: 19px;
-  font-weight: 500;
-  margin: 8px 10px 0px 10px;
+  font-weight: 400;
   cursor: pointer;
+  padding: 25px 30px;
 
   &:hover{
-    background-color: gray;
+    background-color: #F5F5F5;
   }
 `;
 const Service = styled.div`
   height: 200px;
-  padding: 12px 12px;
+  padding: 12px 0px;
+  margin-top: 30px;
 `;
 const MypostArea = styled.div`
   height: 100%;
@@ -123,44 +133,58 @@ const Mypage = (props) => {
   const user_id = localStorage.getItem('user_id');
   const access_token = localStorage.getItem('access_token');
 
+  function onClickQna(){
+    window.location.href = 'https://open.kakao.com/o/g0raUSNg';
+  }
+
   // 로그아웃 클릭 시
   function onClickLogout(){
-    localStorage.removeItem('user_id');
-    localStorage.removeItem('access_token');
+    // eslint-disable-next-line no-restricted-globals
+    const isConfirmed = confirm('정말 로그아웃 하시겠습니까?');
+    
+    if (isConfirmed) {
+        localStorage.removeItem('user_id');
+        localStorage.removeItem('access_token');
 
-    navigate('/');
+        window.location.href = '/';  // 'navigate' 대신 'window.location.href'로 변경 가능
+    }
   }
 
   // 회원탈퇴 클릭 시
   function onClickDeleteUser(){
-    console.log(access_token);
-    axios.post(`/accounts/api/oauth/kakao/signout/`, 
-      {},  // 두 번째 인자로 빈 객체를 전송
-      { headers: { 'Authorization': `Bearer ${access_token}` } }  // 세 번째 인자로 headers 전달
-    )
-    .then(response => {
-        console.log(response.data);
-        localStorage.removeItem('user_id');
-        localStorage.removeItem('access_token');
-        navigate('/');
-      })
-    .catch(error => {
-    console.error(error);
-    });
+    // eslint-disable-next-line no-restricted-globals
+    const isConfirmed = confirm('정말 회원탈퇴를 하시겠습니까?');
+
+    if (isConfirmed) {
+        axios.post(`/accounts/api/oauth/kakao/signout/`, 
+          {},  // 두 번째 인자로 빈 객체를 전송
+          { headers: { 'Authorization': `Bearer ${access_token}` } }  // 세 번째 인자로 headers 전달
+        )
+        .then(response => {
+            //console.log(response.data);
+            localStorage.removeItem('user_id');
+            localStorage.removeItem('access_token');
+            navigate('/');
+          })
+        .catch(error => {
+        console.error(error);
+        });
+    }
+    
   }
 
   // 사용자 정보 가져오는 api
   const [userName, setUserName] = useState();
   const [userProfile, setUserProfile] = useState();
   useEffect(() => {
-    console.log(user_id);
+    //console.log(user_id);
     axios.get(`/mypage/api/${user_id}/inquire`, {
       headers: {
           'Authorization': `Bearer ${access_token}` // 헤더에 access_token 추가
       }
     })
     .then(response => {
-        console.log('user_data',response.data.content.user);
+        //console.log('user_data',response.data.content.user);
         setUserName(response.data.content.user.username);
         setUserProfile(response.data.content.user.profile_photo);
       })
@@ -172,7 +196,7 @@ const Mypage = (props) => {
 
   return (
     <div>
-      <Header pageName = "My Page"/>
+      <Header pageName = "마이페이지"/>
       <MypageContainer id="mypage_container">
         <NameTag id='nametag-area'>
           <ProfileIcon src={userProfile}/>
@@ -186,7 +210,7 @@ const Mypage = (props) => {
         <PostList id='post-area'>
           <PostTitle id='post-title'>
             <Title>내가 쓴 글</Title>
-            <StyledLink href="#"> 더 보기</StyledLink>
+            <StyledLink href="#">더보기</StyledLink>
           </PostTitle>
           <MypostArea>
             <MyPost />
@@ -205,7 +229,7 @@ const Mypage = (props) => {
         <Service>
           <Title>서비스 관리</Title>
           <Line>공지사항</Line>
-          <Line>문의</Line>
+          <Line onClick={onClickQna}>문의</Line>
           <Line onClick={onClickLogout}>로그아웃</Line>
           <Line onClick={onClickDeleteUser}>회원탈퇴</Line>
         </Service>
