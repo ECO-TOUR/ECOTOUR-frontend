340d76f08ededd23de16d23748e4884655956b25
jpg723 <86431761+jpg723@users.noreply.github.com>
2024-09-20 02:26:39 +0900

    [FIX] 시작페이지 디자인 수정

--- src/pages/Onboarding/Intro.js
+++ src/pages/Onboarding/Intro.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import './Intro.css';
-import Ellipse41 from '../../assets/Ellipse 41.svg';
-import Ellipse43 from '../../assets/Ellipse 43.svg';
-import Ellipse44 from '../../assets/Ellipse 44.svg';
+import MockUpImage from '../../assets/MockUpImage.svg';
 import { useNavigate } from 'react-router-dom';
 
 function Intro() {
@@ -16,14 +14,12 @@ function Intro() {
     
   return (
     <div id="intro_container">
-        <img src={Ellipse43} alt="Ellipse 43" className='e43'/>
-        <img src={Ellipse44} alt="Ellipse 44" className='e44'/>
-        <img src={Ellipse41} alt="Ellipse 41" className='e41_intro'/>
         <div class="intro_text">
-            ECO TOUR에 오신 것을 환영해요!<br/>
+            <text style={{ color: "#91EB86", fontWeight: "700" }}>ECO TOUR</text> 에 오신 것을 환영해요!<br/>
             지금 바로 여러분만의<br/>
             맞춤형 생태관광 여행을 계획해보세요!
         </div>
+        <div class="mockup"><object data={MockUpImage} type="image/svg+xml" width="390" hegiht="390"/></div>
         <div id="start_btn" onClick={onClickStartBtn}>시작하기</div>
     </div>
   )
