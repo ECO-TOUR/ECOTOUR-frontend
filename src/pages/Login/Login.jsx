a768aec7b096050f7aee89bc8151692946eafb00
jpg723 <86431761+jpg723@users.noreply.github.com>
2024-09-20 01:51:36 +0900

    [FIX] 로그인 페이지 디자인 변경

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as S from './Login.style';
-import Leaf from '../../assets/Leaf.svg';
+import MockUpImage from '../../assets/MockUpImage.svg';
 import KakaoLoginBtn from '../../assets/kakao_login_medium_wide.svg';
 
 function Login() {
@@ -17,7 +17,8 @@ function Login() {
     <S.Login_container>
       <S.Login_txt>자연으로<br/>떠나는 여행</S.Login_txt>
       <S.Logo>ECO TOUR</S.Logo>
-      <S.Leaf><img src={Leaf}/></S.Leaf>
+      <S.Service_text>자연으로 떠나고 싶은 사람들을 위한<br/>생태 관광 플랫폼</S.Service_text>
+      <S.MockUp><object data={MockUpImage} type="image/svg+xml" width="390" hegiht="390"/></S.MockUp>
       <S.Login_btn src={KakaoLoginBtn} onClick={onclickLoginBtn}/>
     </S.Login_container>
   );
--- src/pages/Login/Login.style.js
+++ src/pages/Login/Login.style.js
@@ -1,51 +1,55 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 export const Login_container = styled.div`
     height: 100vh;
-    background: linear-gradient(180deg, #FFF 8%, #ECFFE9 53%, #91EB86 100%);
+    background-color: white;
+    overflow: hidden;
 `;
 
 /* 로고 위 문구(자연과 함께하는 여행) */
 export const Login_txt = styled.div`
-    padding: 75px 0px 0px 0px;
+    padding: 75px 0px 0px 37px;
     color: #333333;
     font-weight: 700;
     font-size: 28px;
-    text-align: center;
+    text-align: left;
     line-height: 1.5;
+    position: relative;
+    z-index: 1;
 `;
 
 //로고(ECO TOUR)
 export const Logo = styled.div`
-    padding: 20px 0px 0px 0px;
-    text-align: center;
+    padding: 10px 0px 0px 37px;
+    text-align: left;
     font-weight: 900;
     background: linear-gradient(91deg, #A0F096 20.45%, #84D47A 44.85%, #B0FFA7 65.22%, #91EB86 82.2%);
     background-clip: text;
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     font-size: 50px;
-
+    position: relative;
+    z-index: 1;
 `;
 
-// 위아래로 움직이는 애니메이션 정의
-const floatAnimation = keyframes`
-  0% {
-    transform: translateY(0);  /* 초기 위치 */
-  }
-  50% {
-    transform: translateY(-10px);  /* 위로 10px 이동 */
-  }
-  100% {
-    transform: translateY(0);  /* 다시 초기 위치로 */
-  }
+export const Service_text = styled.div`
+    padding-top: 10px;
+    padding-left: 37px;
+    color: #676767;
+    font-size: 15px;
+    line-height: 1.5;
+    font-weight: 300;
+    position: relative;
+    z-index: 1;
 `;
 
-export const Leaf = styled.div`
+export const MockUp = styled.div`
     display: flex;
-    justify-content: center;
-    margin-top: 40px;
-    animation: ${floatAnimation} 1.5s ease-in-out infinite; /* 애니메이션 적용 */
+    justify-content: right;
+    top: 15%;
+    position: fixed;
+    z-index: 0;
+    width: 100%;
 `;
 
 export const Login_btn = styled.img`
